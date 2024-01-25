75a0a4d02cef6862fbe0699d5fdf4e368a918753
Rosne <68449858+rosne-gamingyt@users.noreply.github.com>
2024-01-25 16:12:13 -0500

    Fix payload and temporarily remove CP/KOTH logic

--- Lmaobot/Main.lua
+++ Lmaobot/Main.lua
@@ -144,7 +144,7 @@ local function OnCreateMove(userCmd)
 
     -- Update the current task
     if taskTimer:Run(0.7) then
-        if me:GetHealth() < 100 then
+        if me:GetHealth() < 75 then
             if currentTask ~= Tasks.Health then
                 Log:Info("Switching to health task")
                 Navigation.ClearPath()
@@ -207,20 +207,21 @@ local function OnCreateMove(userCmd)
         end
     else
         -- Generate new path
-        local startNode = Navigation.GetClosestNode(myPos)
-        local goalNode = nil
-
-        if currentTask == Tasks.Objective then
-            local objectives = nil
-
-            -- map check
-            if engine.GetMapName():lower():find("koth_") then
-                -- cp
-                objectives = entities.FindByClass("prop_dynamic")
-            elseif engine.GetMapName():lower():find("pl_") then
-                -- pl
-                objectives = entities.FindByClass("CObjectCartDispenser")
-            elseif engine.GetMapName():lower():find("ctf_") then
+		local startNode = Navigation.GetClosestNode(myPos)
+		local goalNode = nil
+		local entity = nil
+		
+		if currentTask == Tasks.Objective then
+		    local objectives = nil
+		
+		    -- map check
+		    if engine.GetMapName():lower():find("cp_") then
+		        -- cp
+		        objectives = entities.FindByClass("CObjectControlPoint")
+		    elseif engine.GetMapName():lower():find("pl_") then
+		        -- pl
+		        objectives = entities.FindByClass("CObjectCartDispenser")
+		    elseif engine.GetMapName():lower():find("ctf_") then
                 -- ctf
                 local myItem = me:GetPropInt("m_hItem")
                 local flags = entities.FindByClass("CCaptureFlag")
@@ -232,55 +233,65 @@ local function OnCreateMove(userCmd)
                         break
                     end
                 end
-            else
-                Log:Warn("map is NON EXISTANT RETARD")
-                return
-            end
-            
-            -- payload dist check
-            if entity then
-                local distanceToPayload = (myPos - entity:GetAbsOrigin()):Length()
-                local thresholdDistance = 100
-
-                if distanceToPayload > thresholdDistance then
-                    Navigation.FindPath(startNode, goalNode)
-                    currentNodeIndex = #Navigation.GetCurrentPath()
-                end
-            end
-
-            if not goalNode then
-                currentTask = Tasks.Objective
-                Navigation.ClearPath()
-            end
-
-        elseif currentTask == Tasks.Health then
-
-            -- Find the closest health pack
-            local closestDist = math.huge
-            for idx, pos in pairs(healthPacks) do
-                local dist = (myPos - pos):Length()
-                if dist < closestDist then
-                    closestDist = dist
-                    goalNode = Navigation.GetClosestNode(pos)
-                    Log:Info("Found health pack at node %d", goalNode.id)
-                end
-            end
-
-        else
-            Log:Debug("Unknown task: %d", currentTask)
-            return
-        end
-
-        -- Check if we found a start and goal node
-        if not startNode or not goalNode then
-            Log:Warn("Could not find new start or goal node")
-            return
-        end
-
-        -- Update the pathfinder
-        Log:Info("Generating new path from node %d to node %d", startNode.id, goalNode.id)
-        Navigation.FindPath(startNode, goalNode)
-        currentNodeIndex = #Navigation.GetCurrentPath()
+		    else
+		        Log:Warn("Unsupported Gamemode, try CTF or PL")
+		        return
+		    end
+		
+		    -- Iterate through objectives and find the closest one
+		    local closestDist = math.huge
+		    for idx, ent in pairs(objectives) do
+		        local dist = (myPos - ent:GetAbsOrigin()):Length()
+		        if dist < closestDist then
+		            closestDist = dist
+		            goalNode = Navigation.GetClosestNode(ent:GetAbsOrigin())
+		            entity = ent
+		            Log:Info("Found objective at node %d", goalNode.id)
+		        end
+		    end
+		
+		    -- Check if the distance between player and payload is greater than a threshold
+		    if entity then
+		        local distanceToPayload = (myPos - entity:GetAbsOrigin()):Length()
+		        local thresholdDistance = 300
+		
+		        if distanceToPayload > thresholdDistance then
+		            -- If too far, update the path to get closer
+		            Navigation.FindPath(startNode, goalNode)
+		            currentNodeIndex = #Navigation.GetCurrentPath()
+		        end
+		    end
+		
+		    if not goalNode then
+		        Log:Warn("No objectives found. Continuing with default objective task.")
+		        currentTask = Tasks.Objective
+		        Navigation.ClearPath()
+		    end
+		elseif currentTask == Tasks.Health then
+		    local closestDist = math.huge
+		    for idx, pos in pairs(healthPacks) do
+		        local dist = (myPos - pos):Length()
+		        if dist < closestDist then
+		            closestDist = dist
+		            goalNode = Navigation.GetClosestNode(pos)
+		            Log:Info("Found health pack at node %d", goalNode.id)
+		        end
+		    end
+		else
+		    Log:Debug("Unknown task: %d", currentTask)
+		    return
+		end
+		
+		-- Check if we found a start and goal node
+		if not startNode or not goalNode then
+		    Log:Warn("Could not find new start or goal node")
+		    return
+		end
+		
+		-- Update the pathfinder
+		Log:Info("Generating new path from node %d to node %d", startNode.id, goalNode.id)
+		Navigation.FindPath(startNode, goalNode)
+		currentNodeIndex = #Navigation.GetCurrentPath()
     end
 end
 
